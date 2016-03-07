f9f8c1ac5c5b775085eacdbc6624b89efc81807a
Marie <mariethegreat89303@gmail.com>
2016-03-08 03:12:16 +0800

    Working

--- src/cmsc132_mp/CMSC132_MP.java
+++ src/cmsc132_mp/CMSC132_MP.java
@@ -5,6 +5,8 @@
  */
 package cmsc132_mp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MiriamMarie
@@ -15,7 +17,95 @@ public class CMSC132_MP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Input: ");
+        Scanner in = new Scanner(System.in);
+        String temp = in.nextLine();
+        char[] input = temp.toCharArray();
+        char[] finalInput = new char[100];
+        char[][] bigMemory = new char[input.length][4];
+        char[][] smallMemory = new char[input.length][4];
+        int[][] numbMemory = new int[temp.length()][4];
+        int inputSize = input.length;
+        
+        //Copy
+        System.arraycopy(input, 0, finalInput, 0, input.length);
+        
+        if((int) finalInput[0] >= 48 && (int) finalInput[0] <= 57){
+            //For integer inputs
+            String[] ar = temp.split(" ");
+            int [] numbers = new int[ar.length];
+            
+            //Move inputs
+            for(int i=0; i < ar.length; i++){
+                numbers[i] = Integer.valueOf(ar[i]);
+            }
+            
+            System.out.println("BIG ENDIAN & LITTLE ENDIAN");
+            System.out.println("REPRESENTATION FOR INTEGERS");
+            
+            int numberLength = numbers.length;
+            int row = 0, i=0;
+            while(numberLength > 0){
+                for(int column = 0; column < 4; column++){
+                    if(column!=3){ //Check place if rightest or not
+                        numbMemory[row][column] = 0;
+                    } else {
+                        numbMemory[row][column] = numbers[i];
+                        i++;
+                        numberLength--;
+                    }
+                    System.out.print(numbMemory[row][column] + "|");
+                }
+                row++;
+                System.out.println("");
+            }
+        } else { //For string inputs
+            
+            //Check if multiple of four
+            while(!(inputSize % 4 == 0)){
+                finalInput[inputSize] = '0';
+                inputSize++;
+            }   
+            
+            
+            System.out.println("BIG ENDIAN REPRESENTATION");
+            //Initialization
+            int i = 0, k = 0;
+            int tempSize = inputSize;
+            
+            while(tempSize > 0){
+                for(int j=0; j < 4; j++){
+                    bigMemory[k][j] = finalInput[i];
+                    System.out.print(bigMemory[k][j] + "|");
+                    i++;
+                    tempSize--;
+                }
+                k++;
+                System.out.println("");
+            } 
+            System.out.println("---------------");
+            
+            System.out.println("LITTLE ENDIAN REPRESENTATION");
+            //Re initialize
+            int index = 0, row =0;
+            tempSize = inputSize;
+            
+            while(tempSize > 0){
+                int j = 0;
+                //Store reversely
+                for(int column=3; j < 4; j++){
+                    smallMemory[row][column] = finalInput[index];
+                    column--;
+                    index++;
+                    tempSize--;
+                }
+                // Separate printing 
+                for(int m=0; m < 4; m++){
+                    System.out.print(smallMemory[row][m]+"|");
+                }
+                System.out.println("");
+                row++;
+            }
+        }
     }
-    
 }
